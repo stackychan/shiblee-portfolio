732a5cfe806ae51cb3d30f05945512c0b52e047b
Shiblee Showkat <shibleeshowkat@gmail.com>
2026-08-06 19:33:01 +0530

    Remove seconds from clock to fix mobile layout wrapping

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -51,7 +51,6 @@ export default function HeroSection() {
           year: "numeric",
           hour: "2-digit",
           minute: "2-digit",
-          second: "2-digit",
         }),
       );
     };
